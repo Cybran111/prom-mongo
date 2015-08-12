dcc713e879f1df6075958d96850c4379f8527ff7
Nikita Lyubchich <cybran111@gmail.com>
2015-08-12 10:57:10 +0300

    done?

--- task.py
+++ task.py
@@ -1,1 +1,11 @@
-import pymongo
+from pymongo import MongoClient
+client = MongoClient()
+db = client["prom-mongo"]
+
+def extract_data(data):
+  for row in data.split(','):
+      yield from row.split(';')
+
+new_entry = {k: v for k, v in extract_data(sys.argv[1])}
+
+db.entries.insert_one(new_entry)
